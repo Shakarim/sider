025d69394cc3a625ba69928853eafb21bd474771
Shakarim <S.Shakarim@mail.ru>
2023-12-05 20:26:35 +0600

    init: load default app state

---
+++ lib/sider.ex
@@ -0,0 +1,18 @@
+defmodule Sider do
+  @moduledoc """
+  Documentation for `Sider`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sider.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sider_test.exs
@@ -0,0 +1,8 @@
+defmodule SiderTest do
+  use ExUnit.Case
+  doctest Sider
+
+  test "greets the world" do
+    assert Sider.hello() == :world
+  end
+end
